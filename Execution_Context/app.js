9b0037d9427a7a99742626fd8f4e6bd8a6212d43
Colton <kaiserc2@seattleu.edu>
2020-09-07 17:55:24 -0700

    Execution context

---
+++ Execution_Context/app.js
@@ -0,0 +1,14 @@
+b()
+//Returns 'Called b!'
+
+console.log(a)
+//Returns undefined
+
+var a = 'Hello World!'
+
+console.log(a)
+//Returns 'Hello World!'
+
+function b() {
+    console.log('Called b!')
+}
