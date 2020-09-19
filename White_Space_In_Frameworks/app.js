5219967e922471ebfb3fd816b207f1b58bc9524b
Colton <kaiserc2@seattleu.edu>
2020-09-19 14:52:47 -0700

    White space in frameworks

---
+++ White_Space_In_Frameworks/app.js
@@ -0,0 +1,20 @@
+var 
+  //first name of person
+  firstname, 
+  
+  //lastname of person
+  lastname, 
+  
+  //the language
+  //can be 'en' or 'es'
+  language
+
+var person = {
+  firstname: 'John',
+  Lastname: 'Doe'
+}
+
+console.log(person)
+
+//Lots of frameworks and libraries use very liberal whitespace since Javascript doesn't care.
+//You should do this to make your code readable and understandable.
