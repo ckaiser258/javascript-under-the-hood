24355565a0baf1c5ea2bea2814dfdc97675ac34e
Colton <kaiserc2@seattleu.edu>
2020-09-30 17:19:43 -0700

    Add note on if you forget the 'new' keyword to function constr.

--- Function_Constructors_New/app.js
+++ Function_Constructors_New/app.js
@@ -30,3 +30,16 @@ console.log(jane)
 //Putting the new operator in front of a function sets
 //the 'this' keyword to a brand new empty object.
 //And if the function doesn't return anything, it will return the object. Instead of undefined.
+
+//Forgetting to use the 'new' keyword can cause problems.
+var joe = Person('Joe', 'Schmoe')
+//The Person() function is meant to be a function constructor, so it doesn't return anything
+//But function constructors are still functions and we can call them by accident like this.
+//Since Person() doesn't have a return statement, this function returns undefined.
+console.log(joe)
+//Logs undefined since Person doesn't have a 'new' operator in front of it,
+//thus is calling the function that doesn't have a return statement and returns undefined.
+console.log(joe.firstname)
+//Logs an error since joe is set to undefined.
+
+//Always make sure your function constructors start with a capital letter so you can catch these errors beforehand
