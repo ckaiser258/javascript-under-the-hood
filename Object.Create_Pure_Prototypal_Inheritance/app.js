4ff5f09f623161e3710d8882fd7564dc96603ae6
Colton <kaiserc2@seattleu.edu>
2020-10-07 17:04:07 -0700

    Add polyfill for Object.create

--- Object.Create_Pure_Prototypal_Inheritance/app.js
+++ Object.Create_Pure_Prototypal_Inheritance/app.js
@@ -1,3 +1,23 @@
+//Polyfill is code that adds a feature which an older engine may lack.
+//If your browser doesn't have Object.create, this will run.
+//If it does have it, it will be skipped.
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation' + ' only accepts the first parameter.')
+    }
+    //Create an empty function
+    function F() {}
+    //Set the prototype of F() equal to the passed in function
+    F.prototype = o
+    //Return a new constructor function
+    return new F()
+    //1. new creates an empty object
+    //2. runs the empty function
+    //3. points the new prototype of that object to whatever you passed in
+  }
+}
+
 var person = {
   firstname: 'Default',
   lastname: 'Default',
@@ -28,3 +48,7 @@ console.log(john.greet())
 //Logs "Hi John"
 //greet() works because the JavaScript engine looks up the prototypal chain
 //and it finds greet() on john's prototype.
+
+//Pure prototypal inheritance is creating an object,
+//and using that object as the prototype for other objects.
+//Allows us to add and mutate features on the fly.
